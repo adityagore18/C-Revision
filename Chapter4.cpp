414c6ae9cbeb73e12ac0f314b1b0419c43924622
adityagore18 <aditya.gore18@gmail.com>
2025-07-25 10:43:51 +0530

    Chpter 4 is edited

--- Chapter4.cpp
+++ Chapter4.cpp
@@ -8,6 +8,11 @@ using namespace std;
 int add(int a,int b); 
 int sub(int ,int);
 void swap(int &,int &);
+int & max(int & ,int &); 
+//inline function 
+inline double volume(double,double,double);
+//defult argument
+double interest(int,int,double rate=7.5 );
 
 int main(){
     int x=5;
@@ -20,6 +25,16 @@ int main(){
     cout<<"Before Swapping the value of x and y "<<x<<" "<<y<<endl;
     swap(x,y);
     cout<<"Before Swapping the value of x and y "<<x<<" "<<y<<endl;
+
+
+    max(x,y)= -1;
+    cout<<"y is larger "<<y<<endl;
+ 
+    //inline function called;
+    cout<<"volume of cuboid is "<<volume(1.5,1.2,1.3)<<endl;
+
+    cout<<"Interest : "<<interest(10000,5)<<endl;
+    cout<<"Interest : "<<interest(10000,5,10)<<endl;
 
     return 0;
 }
@@ -40,4 +55,22 @@ void swap(int &a ,int &b){
     a=b;
     b=temp;
 }
+
+//return by reference
+int & max(int &a,int &b){
+    if(a>=b) return a;
+    else return b;
+}
+
+//inline function body;
+inline double volume(double l,double b,double h){
+    return l*b*h;
+}
+
+//defult argument
+double interest(int principal,int time,double rate ){
+     return (principal*time*rate)/100;
+
+}
+
 
